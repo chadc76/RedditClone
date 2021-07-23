407ae762b8d17df5adfda13da89269ffd1193e69
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-23 13:55:14 -0400

    Completed Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  before_action :logged_in?, except: [:destroy]
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create 
+    @user = User.find_by_credentials(
+      params[:user][:username]
+      params[:user][:password]
+
+    if @user.nil?
+      flash.now[:errors] = ["username and Password did not match"]
+      render :new
+    else
+      log_in_user!(@user)      
+    end
+  end
+
+  def destroy
+    if !current_user
+      redirect_to new_session_url
+      return
+    end
+
+    log_out_user!
+    redirect_to new_session_url
+  end
+end
