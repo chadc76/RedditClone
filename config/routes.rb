30cde201ae4a2bd67e92167510ae2fec09ad2ca9
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-23 13:38:45 -0400

    Added session and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: %i(new create)
+
+  resources :sessions, only: %i(new create destroy)
 end
