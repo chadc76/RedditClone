088501d2009128a8cb5f9118bbc6d361e96f9ebb
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-23 13:55:06 -0400

    Added helper methods and common methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def log_in_user!(user)
+    session[:session_token] = user.session_token
+    redirect_to user_url(user)
+  end
+
+  def log_out_user!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in? 
+    if current_user
+      redirect_to user_url(current_user)
+      return
+    end
+  end
 end
