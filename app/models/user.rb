946fde7e4c7d3e81983b62661c24237c9e2324bf
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-23 13:38:56 -0400

    Completed User Model

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.generate_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_token
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_token
+  end
+end
