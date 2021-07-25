85b660c547652b1f0fbbe15ace974285d8ea4bdc
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-24 20:34:55 -0400

    Added comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   resources :subs
 
-  resources :posts, except: [:index]
+  resources :posts, except: [:index] do
+    resources :comments, only: [:new]
+  end
+
+  resources :comments, only: [:create]
 end
