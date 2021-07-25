fd2e4e1628da67cd95109d4974aab2e2fbbde667
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-24 21:21:10 -0400

    Added parent and child comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,13 @@
 #
 # Table name: comments
 #
-#  id         :bigint           not null, primary key
-#  content    :text             not null
-#  author_id  :integer          not null
-#  post_id    :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :bigint           not null, primary key
+#  content           :text             not null
+#  author_id         :integer          not null
+#  post_id           :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  parent_comment_id :integer
 #
 class Comment < ApplicationRecord
   validates :content, :author_id, :post_id, presence: true
@@ -15,4 +16,14 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   belongs_to :author, class_name: :User
+
+  has_many :child_comments, 
+    dependent: :destroy,
+    foreign_key: :parent_comment_id,
+    class_name: :Comment
+
+  belongs_to :parent_comment, 
+    optional: true,
+    foreign_key: :parent_comment_id,
+    class_name: :Comment
 end
