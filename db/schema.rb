048afbc831a5db7d33cc91127d49559d93b9d7c4
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-24 20:28:42 -0400

    Added comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_231148) do
+ActiveRecord::Schema.define(version: 2021_07_25_002738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "post_subs", force: :cascade do |t|
     t.integer "post_id", null: false
