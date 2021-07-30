df6f7aa49e05290c41de8e30a69bda5ea85a5763
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-29 22:33:38 -0400

    Created Vote table

---
+++ test/models/vote_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :bigint           not null, primary key
+#  value        :integer          not null
+#  votable_type :string
+#  votable_id   :bigint
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+require "test_helper"
+
+class VoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
