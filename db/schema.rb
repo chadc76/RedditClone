860665b31305e91597feb72c9a38f850989470c6
Chad Cunningham <chadcunningham7676@gmail.com>
2021-07-30 14:06:21 -0400

    Added Subscriptions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_145341) do
+ActiveRecord::Schema.define(version: 2021_07_30_180450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 2021_07_30_145341) do
     t.index ["moderator_id"], name: "index_subs_on_moderator_id"
     t.index ["slug"], name: "index_subs_on_slug", unique: true
     t.index ["title"], name: "index_subs_on_title", unique: true
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "sub_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "sub_id"], name: "index_subscriptions_on_user_id_and_sub_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
